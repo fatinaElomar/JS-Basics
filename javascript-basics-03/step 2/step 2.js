0a1b5f116baeb7cf689e36f229d3089135399f64
fatinaElomar <fatina-el-omar@hotmail.co.uk>
2022-12-08 19:35:27 +0200

    step 3 basics 3

---
+++ javascript-basics-03/step 2/step 2.js
@@ -0,0 +1,14 @@
+var sh = document.getElementById("show");
+var hi = document.getElementById("hide");
+var txt = document.getElementById("texte")
+var a = document.querySelectorAll("a")
+
+a.forEach((e) => {
+    e.addEventListener("click", (e) =>{
+    if(e.target.id == "hide"){
+        txt.style.display = "none"
+    } else {
+        txt.style.display = "block"
+    
+    }
+})});

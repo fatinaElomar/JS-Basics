1b3b3956888b4c3af90beda4caee7e72e5f1412b
fatinaElomar <fatina-el-omar@hotmail.co.uk>
2022-12-08 09:19:00 +0200

    step 5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,10 @@
+let x =prompt("enter your first number")
+let first_number = document.getElementById("first_number")
+first_number.value=x
+
+let y =prompt("enter your second number")
+let second_number = document.getElementById("second_number")
+second_number.value=y
+let validate_button =document.getElementById("validate").onclick=function (){
+    alert( x* y)
+}
